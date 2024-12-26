91ed4b59bbd323262ca9b0552e244c5e9cf35375
Antoinebattini <112547342+Antoinebattini@users.noreply.github.com>
2024-12-26 19:24:52 +0100

    Update Objectif.py
    Ca servait à rien de le laisser la

--- Objectif.py
+++ Objectif.py
@@ -1,4 +1,1 @@
-"""Ici j'écris ce qu'il me reste à faire"""
-# je dois réussir à faire le plot voulu pour remarquer les points d'entrés 
-# Je dois trouver une autre facon de trouver le spread 
-# Je dois créer la 
+
